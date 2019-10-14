4b085d97c0b64ba140be126affd4ddcfb19f2266
Marian Steinbach <marian@giantswarm.io>
2019-10-14 13:18:26 +0200

    More checks

--- cmd/run.go
+++ cmd/run.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"time"
 
+	gsclient "github.com/giantswarm/gsclientgen/client"
 	"github.com/giantswarm/gsclientgen/client/clusters"
 	"github.com/giantswarm/gsclientgen/client/info"
 	"github.com/giantswarm/gsclientgen/models"
 	"github.com/giantswarm/microerror"
+	"github.com/go-openapi/runtime"
 	"github.com/spf13/cobra"
 )
 
@@ -23,21 +25,22 @@ func ExecuteRunCommand(cmd *cobra.Command, args []string) {
 	endpoint, err := cmd.Flags().GetString("endpoint")
 	exitIfError(err)
 
-	scheme, err := cmd.Flags().GetString("scheme")
-	exitIfError(err)
-
-	token, err := cmd.Flags().GetString("token")
-	exitIfError(err)
-
-	authWriter, err := getClientAuth(scheme, token)
-	exitIfError(err)
-
-	//var giantSwarmClient *gsclient.Gsclientgen
-	giantSwarmClient, err := newClient(endpoint)
-	exitIfError(err)
+	var authWriter runtime.ClientAuthInfoWriter
+	var giantSwarmClient *gsclient.Gsclientgen
 	{
-		params := info.NewGetInfoParams()
+		scheme, err := cmd.Flags().GetString("scheme")
+		exitIfError(err)
 
+		token, err := cmd.Flags().GetString("token")
+		exitIfError(err)
+
+		authWriter, err = getClientAuth(scheme, token)
+		exitIfError(err)
+
+		giantSwarmClient, err = newClient(endpoint)
+		exitIfError(err)
+
+		params := info.NewGetInfoParams()
 		infoResponse, err := giantSwarmClient.Info.GetInfo(params, authWriter)
 		exitIfError(err)
 
@@ -50,41 +53,58 @@ func ExecuteRunCommand(cmd *cobra.Command, args []string) {
 	/// 1. Create a cluster with one node pool based on defaults
 	fmt.Printf("\nStep 1 - Create a cluster with one node pool based on defaults\n")
 
-	org := "giantswarm"
-	req := &models.V5AddClusterRequest{Owner: &org}
-	params := clusters.NewAddClusterV5Params().WithBody(req)
-	creationResult, err := giantSwarmClient.Clusters.AddClusterV5(params, authWriter)
-	exitIfError(err)
-
-	clusterOneID := creationResult.Payload.ID
-	printSuccess("Cluster created with ID %s", clusterOneID)
-
+	var clusterOneID string
+	var creationResult *clusters.AddClusterV5Created
 	{
-		// Verify cluster details
-		if creationResult.Payload.Name == "" {
-			complain(microerror.New("Cluster name is empty"))
-		}
-		if creationResult.Payload.APIEndpoint == "" {
-			complain(microerror.New("Cluster api_endpoint is empty"))
-		}
-		if creationResult.Payload.Master == nil {
-			complain(microerror.New("Cluster master is empty"))
-		} else {
-			if creationResult.Payload.Master.AvailabilityZone == "" {
+		org := "giantswarm"
+		req := &models.V5AddClusterRequest{Owner: &org}
+		params := clusters.NewAddClusterV5Params().WithBody(req)
+		creationResult, err = giantSwarmClient.Clusters.AddClusterV5(params, authWriter)
+		exitIfError(err)
+
+		{
+			// Verify cluster details
+			if creationResult.Payload.Name == "" {
+				complain(microerror.New("Cluster name is empty"))
+			}
+			if creationResult.Payload.APIEndpoint == "" {
+				complain(microerror.New("Cluster api_endpoint is empty"))
+			}
+			if creationResult.Payload.Master == nil {
+				complain(microerror.New("Cluster master is empty"))
+			} else if creationResult.Payload.Master.AvailabilityZone == "" {
 				complain(microerror.New("Cluster master.availability_zone is is empty"))
 			}
+			if creationResult.Payload.ReleaseVersion == "" {
+				complain(microerror.New("Cluster release_version is is empty"))
+			}
+			if creationResult.Payload.CreateDate == "" {
+				complain(microerror.New("Cluster create_date is is empty"))
+			}
+			if creationResult.Payload.Owner == "" {
+				complain(microerror.New("Cluster owner is is empty"))
+			}
+
+			if creationResult.Payload.ID == "" {
+				// we can't continue without this
+				exitIfError(microerror.New("Cluster ID is is empty"))
+			}
 		}
+
+		clusterOneID = creationResult.Payload.ID
+		printSuccess("Cluster created with ID %s", clusterOneID)
 	}
 
 	/// 19. Delete the cluster
 	fmt.Printf("\nStep 19 - Delete cluster created in step 1\n")
+	{
+		// wait for some time
+		time.Sleep(5 * time.Second)
 
-	// wait for some time
-	time.Sleep(5 * time.Second)
+		deleteClusterOneParams := clusters.NewDeleteClusterParams().WithClusterID(clusterOneID)
+		_, err = giantSwarmClient.Clusters.DeleteCluster(deleteClusterOneParams, authWriter)
+		exitIfError(err)
 
-	deleteClusterOneParams := clusters.NewDeleteClusterParams().WithClusterID(clusterOneID)
-	_, err = giantSwarmClient.Clusters.DeleteCluster(deleteClusterOneParams, authWriter)
-	exitIfError(err)
-
-	printSuccess("Cluster %s has been deleted", clusterOneID)
+		printSuccess("Cluster %s has been deleted", clusterOneID)
+	}
 }

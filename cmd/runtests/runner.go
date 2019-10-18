3712d5bdd291185361074ebb4224d10d992d1334
Marian Steinbach <marian@giantswarm.io>
2019-10-18 10:11:06 +0200

    Add timestamps to terminal output

--- cmd/runtests/runner.go
+++ cmd/runtests/runner.go
@@ -64,7 +64,7 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 	cliutil.ExitIfError(err)
 
 	// 1. Create a cluster with one node pool based on defaults.
-	fmt.Printf("\nStep 1 - Create a cluster with one node pool based on defaults\n")
+	fmt.Printf("\nStep 1 - Create a cluster with one node pool based on defaults - %s\n", time.Now())
 	clusterOneID, clusterOneAPIEndpoint, err := uat.Test01ClusterCreation(giantSwarmClient, authWriter)
 	cliutil.ExitIfError(err)
 
@@ -72,7 +72,7 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 
 	// Workaround until step 1 returns proper cluster info.
 	if clusterOneAPIEndpoint == "" {
-		fmt.Printf("\nStep 1a - Get cluster details, so we know the API endpoint\n")
+		fmt.Printf("\nStep 1a - Get cluster details, so we know the API endpoint - %s\n", time.Now())
 		var params *clusters.GetClusterV5Params
 		params = clusters.NewGetClusterV5Params().WithClusterID(clusterOneID)
 		result, err := giantSwarmClient.Clusters.GetClusterV5(params, authWriter)
@@ -94,28 +94,28 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 	//time.Sleep(1 * time.Second)
 
 	// Create key pair
-	fmt.Printf("\nStep 6 - Create a key pair for cluster %s with k8s endpoint '%s'\n", clusterOneID, clusterOneAPIEndpoint)
+	fmt.Printf("\nStep 6 - Create a key pair for cluster %s with k8s endpoint '%s' - %s\n", clusterOneID, clusterOneAPIEndpoint, time.Now())
 	kubeconfigPath, err := uat.Test06CreateKeyPair(giantSwarmClient, authWriter, clusterOneID, clusterOneAPIEndpoint)
 	cliutil.ExitIfError(err)
 
-	fmt.Printf("\nStep 7 - Access cluster's K8s API %s with kubeconfig file %s\n(Take your time, we wait until it succeeds.)\n", clusterOneAPIEndpoint, kubeconfigPath)
+	fmt.Printf("\nStep 7 - Access cluster's K8s API %s with kubeconfig file %s - %s\n(Take your time, we wait until it succeeds.)\n", clusterOneAPIEndpoint, kubeconfigPath, time.Now())
 	operation := func() error {
 		return uat.Test07GetKubernetesNodes(kubeconfigPath)
 	}
 	err = backoff.Retry(operation, backoff.NewConstantBackOff(10*time.Second))
 	cliutil.ExitIfError(err)
 
-	// 8. Deploy helloworld
-	fmt.Printf("\nStep 8 - Deploy test app\n")
+	// 8. Deploy test app
+	fmt.Printf("\nStep 8 - Deploy test app\n - %s", time.Now())
 	testAppURL, err := uat.Test08DeployTestApp(kubeconfigPath, clusterOneAPIEndpoint)
 	cliutil.ExitIfError(err)
 
 	// 9. Create load
-	fmt.Printf("\nStep 9 - Create load on test app\n")
+	fmt.Printf("\nStep 9 - Create load on test app - %s\n", time.Now())
 	uat.Test09CreateLoadOnIngress(testAppURL)
 
 	//20. Delete cluster one.
-	// fmt.Printf("\nStep 20 - Delete cluster\n")
+	// fmt.Printf("\nStep 20 - Delete cluster - %s\n", time.Now())
 	// err = uat.Test20ClusterDeletion(giantSwarmClient, authWriter, clusterOneID)
 	// cliutil.ExitIfError(err)
 
